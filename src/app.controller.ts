cfb5902412bd5c34b9f3316943acab084e117b7c
Yasushi Takehara <yasushitakehara@Yasushis-MacBook-Air-2020.local>
2022-05-06 14:19:11 +0900

    upload

---
+++ src/app.controller.ts
@@ -0,0 +1,30 @@
+import { Controller, Get, Render } from "@nestjs/common";
+// import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  // constructor(private readonly appService: AppService) {}
+
+  @Get("/")
+  @Render("index")
+  index() {
+    const viewData = [];
+    viewData["title"] = "Home Page - Online Store";
+    return {
+      viewData: viewData,
+    };
+  }
+
+  @Get("/about")
+  @Render("about")
+  about() {
+    const viewData = [];
+    viewData["title"] = "About us - Online Store";
+    viewData["subtitle"] = "About us";
+    viewData["description"] = "This is an about page ...";
+    viewData["author"] = "Developed by: Yasushi Takehara";
+    return {
+      viewData: viewData,
+    };
+  }
+}
---
+++ src/app.module.ts
@@ -0,0 +1,30 @@
+import { Module, Global } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { ProductsController } from "./products.controller";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { ProductsService } from "./models/products.service";
+import { UsersService } from "./models/users.service";
+import { OrdersService } from "./models/orders.service"
+import { Product } from "./models/product.entity";
+import { User } from "./models/user.entity";
+import { Order } from './models/order.entity';
+import { AdminModule } from "./admin/admin.module";
+import { AuthModule } from "./auth/auth.module";
+import { CartModule } from "./cart/cart.module";
+import { AccountModule } from "./account/account.module";
+
+@Global()
+@Module({
+  imports: [
+    TypeOrmModule.forRoot(),
+    TypeOrmModule.forFeature([Product, User, Order]),
+    AdminModule,
+    AuthModule,
+    CartModule,
+    AccountModule,
+  ],
+  controllers: [AppController, ProductsController],
+  providers: [ProductsService, UsersService, OrdersService],
+  exports: [ProductsService, UsersService, OrdersService],
+})
+export class AppModule {}
